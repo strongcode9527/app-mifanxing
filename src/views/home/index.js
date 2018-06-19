69bdefe556343c74853e3f8c3939e35e7de3b591
李壮 <1477869317@qq.com>
2018-06-19 13:08:31 +0800

    :sparkles:

---
+++ src/views/home/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text style={styles.welcome}>
+          my name is strong
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
